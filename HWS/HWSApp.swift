5c752b00a19aa573600b4722d43497d835897e91
radblesk <144258807+radblesk@users.noreply.github.com>
2025-08-10 20:11:42 +0200

    Initial Commit

---
+++ HWS/HWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSApp.swift
+//  HWS
+//
+//  Created by Radoslav Bley on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct HWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
