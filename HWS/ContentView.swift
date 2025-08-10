92f2bd65630ff0cd753c66b4fba5590574256990
radblesk <144258807+radblesk@users.noreply.github.com>
2025-08-11 00:40:18 +0200

    feat(ContentView): create NavigationSplitView

--- HWS/ContentView.swift
+++ HWS/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var columnVisibility = NavigationSplitViewVisibility
+        .all
+    @State private var preferredCompactColumn = NavigationSplitViewColumn
+        .sidebar
+    @State private var selectedDays: Int?
+    @State private var selectedProject: Int?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationSplitView(
+            columnVisibility: $columnVisibility,
+            preferredCompactColumn: $preferredCompactColumn
+        ) {
+            DaysList(selectedDays: $selectedDays)
+                .modelContainer(ProjectsData.shared.modelContainer)
+
+        } content: {
+            if let selectedDays {
+                ProjectsList(
+                    selectedProject: $selectedProject,
+                    selectedDays: selectedDays
+                )
+                .modelContainer(ProjectsData.shared.modelContainer)
+
+            } else {
+                Text("Select course")
+            }
+        } detail: {
+            // Group the views so you can apply a modifier once.
+            Group {
+                // Use a switch statement for better readability.
+                switch selectedProject {
+                case 1:
+                    WeSplit()
+                case 2:
+                    TempConvert()
+                case 3:
+                    GuessTheFlag()
+                default:
+                    Text("Select project")
+                }
+            }
+            // Apply the conditional modifier only once.
+            #if os(macOS)
+                .padding()
+            #endif
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(ProjectsData.shared.modelContainer)
 }
