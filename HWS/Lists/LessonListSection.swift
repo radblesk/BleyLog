87288e57b87da68a13cc7b795dd464def9e3a175
radblesk <144258807+radblesk@users.noreply.github.com>
2025-08-13 20:51:25 +0200

    feat(LessonListSection): add inProgress condition

--- HWS/Lists/LessonListSection.swift
+++ HWS/Lists/LessonListSection.swift
@@ -7,7 +7,9 @@
 
 import SwiftUI
 
-struct LessonsListSection: View {
+struct LessonListSection: View {
+    @Environment(\.modelContext) private var context
+
     var lessons: [Lesson]
     var course: Course
 
@@ -17,16 +19,70 @@ struct LessonsListSection: View {
         Section(course.title, isExpanded: $isExpanded) {
             ForEach(lessons.sorted { $0.firstDay < $1.firstDay }) { lesson in
                 HStack {
-                    if lesson.finished {
+                    if lesson.inProgress {
+                        ProgressView()
+                            .tint(.orange)
+                    } else if lesson.finished {
                         Image(systemName: "checkmark")
                             .foregroundStyle(.green)
                     }
                     Text(
                         "Days \(lesson.firstDay)-\(lesson.lastDay): \(lesson.title)"
                     )
+
+                    Spacer()
+
+                    Image(systemName: "chevron.right")
+                        .foregroundStyle(.secondary)
+                        .imageScale(.small)
+                }
+                .swipeActions(edge: .leading) {
+                    Button(
+                        "Mark as done",
+                        systemImage: lesson.finished ? "xmark" : "checkmark"
+                    ) {
+                        if lesson.inProgress {
+                            lesson.inProgress.toggle()
+                        }
+
+                        lesson.finished.toggle()
+
+                        do {
+                            try context.save()
+                        } catch {
+                            print(error)
+                        }
+                    }
+                    .tint(.green)
+                    Button(
+                        "Mark as in progress",
+                        systemImage: "arrow.triangle.2.circlepath"
+                    ) {
+                        if lesson.finished {
+                            lesson.finished.toggle()
+                        }
+                        lesson.inProgress.toggle()
+
+                        do {
+                            try context.save()
+                        } catch {
+                            print(error)
+                        }
+                    }
+                    .tint(.orange)
                 }
                 .tag(lesson)
             }
         }
+    }
+}
+
+#Preview {
+    List {
+        LessonListSection(
+            lessons: Course.coursesData.first!.lessons,
+            course: Course
+                .coursesData.first!
+        )
     }
 }
