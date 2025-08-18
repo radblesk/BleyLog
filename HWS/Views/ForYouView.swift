11008c897960aa7f257c96fea1a8db98ef16029c
radblesk <144258807+radblesk@users.noreply.github.com>
2025-08-18 02:10:06 +0200

    feat(ForYou): add search function

--- HWS/Views/ForYouView.swift
+++ HWS/Views/ForYouView.swift
@@ -10,34 +10,80 @@ import SwiftUI
 
 struct ForYouView: View {
     @Query private var lessons: [Lesson]
+    @State private var searchText: String = ""
 
     var body: some View {
         NavigationStack {
             List {
                 let lessonsForYou = lessons.filter { $0.inProgress == true }
-                if lessonsForYou.count > 0 {
-                    ForEach(
-                        lessons.filter { $0.inProgress == true },
-                        id: \.self
-                    ) {
-                        lesson in
-                        NavigationLink {
-                            ProjectList(
-                                selectedLesson: lesson,
-                                selectedProject: .constant(nil)
-                            )
-                        } label: {
-                            Label(
-                                "Days \(lesson.firstDay)-\(lesson.lastDay): \(lesson.title)",
-                                systemImage: "book.pages.fill"
-                            )
+                let filteredLessons = lessonsForYou.filter {
+                    $0.title.lowercased().contains(searchText.lowercased())
+                }
+
+                if searchText.isEmpty {
+                    Section {
+                        HStack {
+                            Spacer()
+                            VStack {
+                                Image(systemName: "magnifyingglass")
+                                    .resizable()
+                                    .frame(width: 32, height: 32)
+                                    .padding(8)
+                                    .background(.blue)
+                                    .clipShape(.rect(cornerRadius: 10))
+                                Text(
+                                    "All your in progress lessons in one place. Browse, search and start working."
+                                )
+                                .multilineTextAlignment(.center)
+                                .font(.subheadline)
+                            }
+                            Spacer()
                         }
                     }
+                    if lessonsForYou.count > 0 {
+                        Section("Lessons in progress") {
+                            ForEach(
+                                lessonsForYou,
+                                id: \.self
+                            ) {
+                                lesson in
+                                NavigationLink {
+                                    ProjectList(lesson: lesson)
+                                } label: {
+                                    Label(
+                                        "Days \(lesson.firstDay)-\(lesson.lastDay): \(lesson.title)",
+                                        systemImage: "circle.dotted"
+                                    )
+                                }
+                            }
+                        }
+                    } else {
+                        Text("No lessons in progress...")
+                            .foregroundStyle(.secondary)
+                    }
                 } else {
-                    Text("Nothing to show...")
-                        .foregroundStyle(.secondary)
+                    Text("Searching in \(lessonsForYou.count) lessons...")
+                    if filteredLessons.count > 0 {
+                        ForEach(
+                            lessonsForYou,
+                            id: \.self
+                        ) {
+                            lesson in
+                            NavigationLink {
+                                ProjectList(lesson: lesson)
+                            } label: {
+                                Label(
+                                    "Days \(lesson.firstDay)-\(lesson.lastDay): \(lesson.title)",
+                                    systemImage: "circle.dotted"
+                                )
+                            }
+                        }
+                    } else {
+                        Text("No results for \"\(searchText)\"")
+                    }
                 }
             }
+            .searchable(text: $searchText)
             .navigationTitle("For You")
         }
     }
