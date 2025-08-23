ab8ccecacb40483ebe10fe01a49e2e3be054ae44
radblesk <144258807+radblesk@users.noreply.github.com>
2025-08-23 20:31:51 +0200

    feat(NavigationSplitView): separate NavigationSplitView from ContentView

--- BleyLog/ContentView.swift
+++ BleyLog/ContentView.swift
@@ -9,44 +9,8 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-    // DataModel
-    @Environment(\.modelContext) private var context
-    @Query(sort: \Language.title) private var languages: [Language]
-
-    // States
-    @State private var columnVisibility = NavigationSplitViewVisibility
-        .doubleColumn
-    @State private var preferredCompactColumn = NavigationSplitViewColumn
-        .sidebar
-    @State private var selectedLanguage: Language?
-    @State private var selectedLesson: Lesson?
-    @State private var selectedProject: Project?
-
     var body: some View {
-        NavigationSplitView(
-            columnVisibility: $columnVisibility,
-            preferredCompactColumn: $preferredCompactColumn
-        ) {
-            LessonList(
-                languages: languages,
-                selectedLanguage: $selectedLanguage,
-                lesson: $selectedLesson,
-            )
-            .navigationSplitViewColumnWidth(min: 250, ideal: 300)
-
-        } content: {
-            ProjectList(lesson: selectedLesson, project: $selectedProject)
-                .navigationSplitViewColumnWidth(min: 300, ideal: 300)
-                .onChange(of: selectedLesson) {
-                    columnVisibility = .doubleColumn
-                }
-        } detail: {
-            ProjectDetailView(project: selectedProject)
-        }
-        .onChange(of: selectedLesson) {
-            selectedProject = nil
-        }
-        .navigationSplitViewStyle(.balanced)
+        CoursesSplitView()
     }
 }
 
