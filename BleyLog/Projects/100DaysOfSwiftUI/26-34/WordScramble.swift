22afc8403cf9314bc819452b83c2d866323ce9fe
radblesk <144258807+radblesk@users.noreply.github.com>
2025-08-26 12:06:25 +0200

    feat: created a project file

---
+++ BleyLog/Projects/100DaysOfSwiftUI/26-34/WordScramble.swift
@@ -0,0 +1,18 @@
+//
+//  WordScramble.swift
+//  BleyLog
+//
+//  Created by Radoslav Bley on 26/08/2025.
+//
+
+import SwiftUI
+
+struct WordScramble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WordScramble()
+}
