8fdc7150c0edfcae6de0b2f11085b5907c42590b
radblesk <144258807+radblesk@users.noreply.github.com>
2025-08-31 16:33:13 +0200

    feat(SwiftDataProject): added ability to remove users

--- BleyLog/Projects/100DaysOfSwiftUI/49-59/SwiftDataProject.swift
+++ BleyLog/Projects/100DaysOfSwiftUI/49-59/SwiftDataProject.swift
@@ -26,28 +26,8 @@ struct SwiftDataProject: View {
             .searchable(text: $searchText, prompt: "Search users")
             .toolbar {
                 Button("Add user", systemImage: "plus") {
-//                    try? modelContext.delete(model: User.self)
-//
-//                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
-//                    let second = User(
-//                        name: "Rosa Diaz",
-//                        city: "New York",
-//                        joinDate: .now.addingTimeInterval(86400 * -5)
-//                    )
-//                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
-//                    let fourth = User(
-//                        name: "Johnny English",
-//                        city: "London",
-//                        joinDate: .now.addingTimeInterval(86400 * 10)
-//                    )
-//
-//                    modelContext.insert(first)
-//                    modelContext.insert(second)
-//                    modelContext.insert(third)
-//                    modelContext.insert(fourth)
-                    
                     let newUser = User(name: "New user", city: "", joinDate: .now)
-                    
+
                     modelContext.insert(newUser)
                 }
                 #if os(iOS)
